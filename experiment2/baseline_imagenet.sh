235eb61b5891e21ee12d59302e8fb4fe136f755d
JianJiang_Omen <jian.jiang@kcl.ac.uk>
2022-01-22 14:58:13 +0000

    first commit for running baseline imagenet in beginator

--- experiment2/baseline_imagenet.sh
+++ experiment2/baseline_imagenet.sh
@@ -48,26 +48,25 @@ PATH_DATA='/vol/jj/dataset/KM_dataset'
 # ResNet50 pretrained on ImageNet
 #echo {\"imagenet\": \"0.7616\"} > logs/baseline_imagenet_acc_${ARCH}.txt
 
-#for TASK_ID in `seq 2 6`; do
-#for TASK_ID in 2 6; do
-#    CUDA_VISIBLE_DEVICES=$GPU_ID python packnet_imagenet_main.py \
-#        --arch $ARCH \
-#        --path $PATH_DATA \
-#        --dataset ${DATASETS[TASK_ID]} --num_classes ${NUM_CLASSES[TASK_ID]} \
-#        --lr ${INIT_LR[TASK_ID]} \
-#        --weight_decay 4e-5 \
-#        --save_folder checkpoints/baseline/experiment2/$ARCH/${DATASETS[TASK_ID]} \
-#        --epochs $FINETUNE_EPOCHS \
-#        --mode finetune \
-#        --logfile logs/baseline_imagenet_acc_${ARCH}.txt \
-#        --use_imagenet_pretrained
-#done
-
-for HISTORY_ID in `seq 2 6`; do
+for TASK_ID in `seq 2 6`; do
     CUDA_VISIBLE_DEVICES=$GPU_ID python packnet_imagenet_main.py \
         --arch $ARCH \
         --path $PATH_DATA \
-        --dataset ${DATASETS[HISTORY_ID]} --num_classes ${NUM_CLASSES[TASK_ID]} \
-        --load_folder checkpoints/baseline/experiment2/$ARCH/${DATASETS[HISTORY_ID]} \
-        --mode inference
+        --dataset ${DATASETS[TASK_ID]} --num_classes ${NUM_CLASSES[TASK_ID]} \
+        --lr ${INIT_LR[TASK_ID]} \
+        --weight_decay 4e-5 \
+        --save_folder checkpoints/baseline/experiment2/$ARCH/${DATASETS[TASK_ID]} \
+        --epochs $FINETUNE_EPOCHS \
+        --mode finetune \
+        --logfile logs/baseline_imagenet_acc_${ARCH}.txt \
+        --use_imagenet_pretrained
 done
+
+#for HISTORY_ID in `seq 2 6`; do
+#    CUDA_VISIBLE_DEVICES=$GPU_ID python packnet_imagenet_main.py \
+#        --arch $ARCH \
+#        --path $PATH_DATA \
+#        --dataset ${DATASETS[HISTORY_ID]} --num_classes ${NUM_CLASSES[TASK_ID]} \
+#        --load_folder checkpoints/baseline/experiment2/$ARCH/${DATASETS[HISTORY_ID]} \
+#        --mode inference
+#done
