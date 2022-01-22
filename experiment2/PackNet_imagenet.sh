5800e930548f60403561a562c9733b8fa75bf536
JianJiang_Omen <jian.jiang@kcl.ac.uk>
2022-01-22 11:12:28 +0000

    first commit for running baseline imagenet in beginator

--- experiment2/PackNet_imagenet.sh
+++ experiment2/PackNet_imagenet.sh
@@ -32,13 +32,15 @@ GPU_ID=0
 one_shot_prune_perc=0.6
 
 arch='resnet18'
-finetune_epochs=100
-prune_epochs=30
+#finetune_epochs=100
+#prune_epochs=30
+finetune_epochs=1
+prune_epochs=1
 
 PATH_DATA='/vol/jj/dataset/KM_dataset'
 
-for TASK_ID in `seq 1 4`; do
-  if [ "$TASK_ID" != "1" ]
+for TASK_ID in `seq 0 4`; do
+  if [ "$TASK_ID" != "0" ]
   then
       CUDA_VISIBLE_DEVICES=$GPU_ID python packnet_imagenet_main.py \
           --arch $arch \
@@ -46,8 +48,8 @@ for TASK_ID in `seq 1 4`; do
           --dataset ${DATASETS[TASK_ID]} --num_classes ${NUM_CLASSES[TASK_ID]} \
         --lr ${INIT_LR[TASK_ID]} \
           --weight_decay 4e-5 \
-          --save_folder checkpoints/PackNet/experiment1/$arch/${DATASETS[TASK_ID]}/scratch \
-          --load_folder checkpoints/PackNet/experiment1/$arch/${DATASETS[TASK_ID-1]}/one_shot_prune \
+          --save_folder checkpoints/PackNet/experiment2/$arch/${DATASETS[TASK_ID]}/scratch \
+          --load_folder checkpoints/PackNet/experiment2/$arch/${DATASETS[TASK_ID-1]}/one_shot_prune \
           --epochs $finetune_epochs \
 
   else
@@ -57,7 +59,7 @@ for TASK_ID in `seq 1 4`; do
           --dataset ${DATASETS[TASK_ID]} --num_classes ${NUM_CLASSES[TASK_ID]} \
         --lr ${INIT_LR[TASK_ID]} \
           --weight_decay 4e-5 \
-          --save_folder checkpoints/PackNet/experiment1/$arch/${DATASETS[TASK_ID]}/scratch \
+          --save_folder checkpoints/PackNet/experiment2/$arch/${DATASETS[TASK_ID]}/scratch \
           --epochs $finetune_epochs \
           --use_imagenet_pretrained \
           --mode finetune
@@ -71,8 +73,8 @@ for TASK_ID in `seq 1 4`; do
       --dataset ${DATASETS[TASK_ID]} --num_classes ${NUM_CLASSES[TASK_ID]} \
       --lr ${INIT_LR[TASK_ID]} \
       --weight_decay 4e-5 \
-      --save_folder checkpoints/PackNet/experiment1/$arch/${DATASETS[TASK_ID]}/one_shot_prune \
-      --load_folder checkpoints/PackNet/experiment1/$arch/${DATASETS[TASK_ID]}/scratch \
+      --save_folder checkpoints/PackNet/experiment2/$arch/${DATASETS[TASK_ID]}/one_shot_prune \
+      --load_folder checkpoints/PackNet/experiment2/$arch/${DATASETS[TASK_ID]}/scratch \
       --epochs $prune_epochs \
       --mode prune \
       --one_shot_prune_perc $one_shot_prune_perc
@@ -80,12 +82,12 @@ done
 
 
 # Evaluate tasks
-for history_id in `seq 2 6`; do
-    CUDA_VISIBLE_DEVICES=$GPU_ID python packnet_cifar100_main_normal.py \
+for history_id in `seq 0 4`; do
+    CUDA_VISIBLE_DEVICES=$GPU_ID python packnet_imagenet_main.py \
         --arch $arch \
         --path $PATH_DATA \
         --dataset ${DATASETS[history_id]} --num_classes ${NUM_CLASSES[TASK_ID]} \
-        --load_folder checkpoints/PackNet/experiment1/$arch/${DATASETS[history_id]}/one_shot_prune \
+        --load_folder checkpoints/PackNet/experiment2/$arch/${DATASETS[history_id]}/one_shot_prune \
         --mode inference \
         --logfile logs/PackNet_imagenet.txt
 done
